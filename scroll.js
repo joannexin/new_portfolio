0664b8bd9c54cc4afc2261cd9c62968124d507e0
Joanne <jeffiefeifei@gmail.com>
2017-01-28 10:41:58 -0800

    [add] navbar after reaching out about section

--- scroll.js
+++ scroll.js
@@ -1,49 +1,49 @@
 $(window).scroll(function() {
 
   $('.selfie').each(function(){
-	var imagePos = $(this).offset().top;
+    var imagePos = $(this).offset().top;
 
-	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+400) {
-			$(this).addClass("fadeIn");
-		}
-	});
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+400) {
+      $(this).addClass("fadeIn");
+    }
+  });
 
   $('#aboutme').each(function(){
-	var imagePos = $(this).offset().top;
+    var imagePos = $(this).offset().top;
 
-	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+400) {
-			$(this).addClass("stretchLeft");
-		}
-	});
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+400) {
+      $(this).addClass("stretchLeft");
+    }
+  });
 
   $('.btn-custom').each(function(){
-	var imagePos = $(this).offset().top;
+    var imagePos = $(this).offset().top;
 
-	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+650) {
-			$(this).addClass("tossing");
-		}
-	});
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+650) {
+      $(this).addClass("tossing");
+    }
+  });
 
   $('.timeline-panel-left').each(function(){
-	var imagePos = $(this).offset().top;
+    var imagePos = $(this).offset().top;
 
-	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+500) {
-			$(this).addClass("slideLeft");
-		}
-	});
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+500) {
+      $(this).addClass("slideLeft");
+    }
+  });
 
   $('.timeline-panel-right').each(function(){
-	var imagePos = $(this).offset().top;
+    var imagePos = $(this).offset().top;
 
-	var topOfWindow = $(window).scrollTop();
-		if (imagePos < topOfWindow+500) {
-			$(this).addClass("slideRight");
-		}
-	});
+    var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+500) {
+      $(this).addClass("slideRight");
+    }
+  });
 });
 
 
@@ -76,5 +76,19 @@ $(document).ready(function(){
         window.location.hash = hash;
       });
     }
+  });
+});
+
+// navbar shows after reach about section
+$(document).ready(function(){
+  $(".navbar").hide();
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 720) {
+        $('.navbar').fadeIn();
+      } else {
+        $('.navbar').fadeOut();
+      }
+    });
   });
 });
